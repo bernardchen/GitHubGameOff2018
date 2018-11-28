eefab54563a277745adf34d3c4f568c4625b25d8
Bernard Chen <b.chen25@yahoo.com>
2018-11-28 00:08:56 -0800

    functional-ish title screen

--- TitleScreen/TitleScreen.gd
+++ TitleScreen/TitleScreen.gd
@@ -8,9 +8,10 @@ func _ready():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 
 func _on_Button_pressed(scene_to_load):
-	scene_path_to_load = scene_to_load
 	$FadeIn.show()
 	$FadeIn.fade_in()
+	scene_path_to_load = scene_to_load
 
 func _on_FadeIn_fade_finished():
+	$FadeIn.hide()
 	get_tree().change_scene(scene_path_to_load)
